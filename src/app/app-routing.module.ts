ecabce93b1a6748951429cc7c1416d517f6f8d26
Bhushan Gandla <bhushan.gandla@svclnk.com>
2024-02-28 12:58:09 -0500

    Added new component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HardcodedFormsComponent } from './forms/hardcoded-forms/hardcoded-forms.component';
 import { DynamicFormsComponent } from './forms/dynamic-forms/dynamic-forms.component';
+import { DynamicReactiveFormsComponent } from './forms/dynamic-reactive-forms/dynamic-reactive-forms.component';
 
 const routes: Routes = [
   { path: 'hardcoded-forms', component: HardcodedFormsComponent, data: { title: 'Hardcoded Forms POC' } },
   { path: 'dynamic-forms', component: DynamicFormsComponent, data: { title: 'Dynamic Forms POC' } },
+  { path: 'dynamic-reactive-forms', component: DynamicReactiveFormsComponent, data: { title: 'Dynamic Reactive Forms POC' } },
 ];
 
 @NgModule({
