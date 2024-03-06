4858bb02c90ba7307023b88d658ea54e63e03d17
Bhushan Gandla <bhushan.gandla@svclnk.com>
2024-03-05 23:43:07 -0500

    Added logic to handle No question

--- src/app/forms/dynamic-reactive-forms/dynamic-reactive-forms.component.ts
+++ src/app/forms/dynamic-reactive-forms/dynamic-reactive-forms.component.ts
@@ -10,8 +10,7 @@ import { DynamicReactiveFormService } from 'src/app/shared/dynamic-reactive-form
 export class DynamicReactiveFormsComponent implements OnInit {
   // formDataJson: any;
   formQuestions: any;
-  radioSubQuestionObject: any;
-  addressFlag: boolean = false;
+  subRootQuestionsArray: any = [];
 
   public myForm: FormGroup = this.fb.group({});
 
@@ -28,14 +27,27 @@ export class DynamicReactiveFormsComponent implements OnInit {
     });
   }
 
+
   createForm(questions: any){
     for(const question of questions){
-      this.myForm.addControl(question['QuestionId'], new FormControl('', Validators.required));
+      if(question.FieldType == 'text'){
+        this.myForm.addControl(question['QuestionId'], new FormControl('Sample text', Validators.required));
+      }
+      if(question.FieldType == 'date'){
+        this.myForm.addControl(question['QuestionId'], new FormControl(new Date(2023, 9, 20), Validators.required));
+      }
+      if(question.FieldType == 'number'){
+        this.myForm.addControl(question['QuestionId'], new FormControl('Sample text', Validators.required));
+      }
 
       if(question.FieldType == 'radio'){
         for(const radioQuestion of question['Answers']){
+
+          this.myForm.addControl(question['QuestionId'], new FormControl(radioQuestion['IsChosenAnswer'], Validators.required));
+
+          // console.log(radioQuestion['AnswerId']);
+
           for(const radioSubQuestion of radioQuestion['Questions']){
-            // console.log(radioSubQuestion);
             this.myForm.addControl(radioSubQuestion['QuestionId'], new FormControl('', Validators.required));
           }
           // this.myForm.addControl(question['QuestionId'], new FormControl('', Validators.required));
@@ -51,21 +63,67 @@ export class DynamicReactiveFormsComponent implements OnInit {
     console.log(this.myForm);
   }
 
-  onRadioButtonChange(radioAddressValue: any, radioSubQuestionObject: any){
-    console.log(radioSubQuestionObject);
-    for(const radioQuestions of radioSubQuestionObject.Answers){
-      if(radioQuestions.Questions.length !== 0){
-        this.radioSubQuestionObject = radioQuestions.Questions;
-        console.log(this.radioSubQuestionObject);
-      }else if (radioQuestions.Questions.length === 0){
-        // this.radioSubQuestionObject = [];
+  onRadioButtonChange(subRootQuestionIndex: any, rootQuestionObject: any, subRootQuestionIdInput: any){
+    // for creating and pushing sub questions
+    for(const questionObjectAnswer of rootQuestionObject.Answers){
+
+      // console.log(questionObjectAnswer.AnswerId);
+      // console.log(questionObjectAnswer);
+     
+      // set values to update original object with IsChoseAnswer attribute
+      if(questionObjectAnswer.AnswerId === subRootQuestionIdInput){
+        questionObjectAnswer.IsChosenAnswer = true;
+      }else{
+        questionObjectAnswer.IsChosenAnswer = false;
       }
+      
+      for(const subRootQuestionObject of questionObjectAnswer.Questions){
+        // add controls on html and component class if we find inner Question object filled with sub questions
+        if(questionObjectAnswer.Questions && questionObjectAnswer.AnswerId === subRootQuestionIdInput){
+          subRootQuestionIndex++;
+  
+          // adding new form controls to the myForm
+          this.myForm.addControl(subRootQuestionObject['QuestionId'], new FormControl('Addresses', Validators.required));
+  
+          // adding new objects to formQuestions array
+          this.formQuestions.splice(subRootQuestionIndex, 0, subRootQuestionObject);
+        }
+      }
+
+
+      // for removing sub questions
+      if(questionObjectAnswer.IsChosenAnswer === true && questionObjectAnswer.Questions.length == 0 ){
+        
+        for(const questionObjectAnswer of rootQuestionObject.Answers){
+          // finding which subRootQ-1 has nested subRootQ-2 
+          if(questionObjectAnswer.Questions.length > 0){
+
+            // looping through subRootQ-2
+            for(const subRootQuestionObject of questionObjectAnswer.Questions){
+
+              console.log(subRootQuestionObject.QuestionId);
+
+              const index = this.formQuestions.findIndex((item: any) => item.QuestionId === subRootQuestionObject.QuestionId);
+              if (index !== -1) {
+                // removing subRootQ-2 from formQuestions array
+                this.formQuestions.splice(index, 1);
+                // removing subRootQ-2 from myForm 
+                this.myForm.removeControl(subRootQuestionObject.QuestionId);
+              }
+
+
+            }
+          }
+        }
+
+      }else{
+        // questionObjectAnswer.IsChosenAnswer = false;
+      }
+      
+
     }
-    // this.myForm.addControl(this.radioSubQuestionObject.QuestionId, new FormControl('', Validators.required));
-    // if(radioAddressValue == 'No'){
-    //   this.addressFlag = true;
-    // }else if (radioAddressValue == 'Yes'){
-    //   this.addressFlag = false;
-    // }
+
+    console.log(this.formQuestions);
+
   }
 }
