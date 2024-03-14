db5cd04fd3f578768554979ba1f07d13d8846d07
Bhushan Gandla <bhushan.gandla@svclnk.com>
2024-03-14 14:34:18 -0400

    Order creation UI

--- src/app/forms/dynamic-reactive-forms/dynamic-reactive-forms.component.ts
+++ src/app/forms/dynamic-reactive-forms/dynamic-reactive-forms.component.ts
@@ -40,25 +40,28 @@ export class DynamicReactiveFormsComponent implements OnInit {
       if(question.uiControlType == 'number'){
         this.myForm.addControl(question.questionId, new FormControl('Sample text', Validators.required));
       }
+      if(question.uiControlType == 'select'){
+        this.myForm.addControl(question.questionId, new FormControl(Validators.required));
+      }
 
       if(question.uiControlType == 'radio'){
         for(const radioQuestion of question.answers){
 
-          console.log(radioQuestion.isChosenAnswer);
+          // console.log(radioQuestion.isChosenAnswer);
 
           this.myForm.addControl(question.questionId, new FormControl(Validators.required));
           // setting value
           // this.myForm.get(question.questionId)!.setValue(radioQuestion.isChosenAnswer);
 
 
-          for(const radioSubQuestion of radioQuestion.questions){
-            this.myForm.addControl(radioSubQuestion.questionId, new FormControl('', Validators.required));
-          }
+          // for(const radioSubQuestion of radioQuestion.questions){
+          //   this.myForm.addControl(radioSubQuestion.questionId, new FormControl('', Validators.required));
+          // }
           // this.myForm.addControl(question.questionId, new FormControl('', Validators.required));
         }
       }
     }
-    console.log(this.myForm);
+    // console.log(this.myForm);
     
   }
 
@@ -67,58 +70,96 @@ export class DynamicReactiveFormsComponent implements OnInit {
     console.log(this.myForm);
   }
 
-  onRadioButtonChange(subRootQuestionIndex: any, subRootQuestionObject: any, subRootQuestionIdInput: any){
-    console.log(subRootQuestionObject);
+  onRadioButtonChange(rootQuestionIndex: any, rootQuestionObject: any, rootQuestionIdInput: any){
+    console.log(rootQuestionObject  );
+    // console.log(rootQuestionObject);
     // for creating and pushing sub questions
-    for(const questionObjectAnswer of subRootQuestionObject){
+    const subRootQuestionAnswerArray = rootQuestionObject.answers;
+    for(const questionObjectAnswer of subRootQuestionAnswerArray){
 
       // console.log(questionObjectAnswer.answerId);
       // console.log(questionObjectAnswer);
      
       // set values to update original object with IsChoseAnswer attribute
-      if(questionObjectAnswer.answerId === subRootQuestionIdInput){
+      if(questionObjectAnswer.answerId === rootQuestionIdInput){
         questionObjectAnswer.isChosenAnswer = true;
       }else{
         questionObjectAnswer.isChosenAnswer = false;
       }
       
+      
       for(const subRootQuestionObject1 of questionObjectAnswer.questions){
 
+        if(questionObjectAnswer.isChosenAnswer === true){
         // if selected object is true we are adding question objects
-        if(questionObjectAnswer.isChosenAnswer === true && questionObjectAnswer.answerId === subRootQuestionIdInput){
 
           // add controls on html and component class if we find inner Question object filled with sub questions
-          subRootQuestionIndex++;
+          rootQuestionIndex++;
 
           // adding new objects to formQuestions array
-          this.formQuestions.splice(subRootQuestionIndex, 0, subRootQuestionObject1);
+          this.formQuestions.splice(rootQuestionIndex, 0, subRootQuestionObject1);
           
-          console.log(subRootQuestionObject1);
+          // console.log(subRootQuestionObject1);
           // adding new form controls to the myForm
           this.myForm.addControl(subRootQuestionObject1.questionId, new FormControl(Validators.required));
           // setting value
           this.myForm.get(subRootQuestionObject1.questionId)!.setValue(subRootQuestionObject1.answers[0].answerText);
           
           // check if there is empty array, if it doesn't exist, create a new array 
-          if(!this.subRootQuestionsFlatArray[questionObjectAnswer.answerId]){
-            this.subRootQuestionsFlatArray[questionObjectAnswer.answerId] = [];
+          // console.log(rootQuestionObject.questionId+"-"+questionObjectAnswer.answerId)
+          if(!this.subRootQuestionsFlatArray[rootQuestionObject.questionId+"-"+questionObjectAnswer.answerId]){
+            this.subRootQuestionsFlatArray[rootQuestionObject.questionId+"-"+questionObjectAnswer.answerId] = [];
           }
 
           // do not duplicate existing subRootQuestion questionIds
-          if (!this.subRootQuestionsFlatArray[questionObjectAnswer.answerId].includes(subRootQuestionObject1.questionId)) {
-            this.subRootQuestionsFlatArray[questionObjectAnswer.answerId].push(subRootQuestionObject1.questionId);
+          if (!this.subRootQuestionsFlatArray[rootQuestionObject.questionId+"-"+questionObjectAnswer.answerId].includes(subRootQuestionObject1.questionId)) {
+            this.subRootQuestionsFlatArray[rootQuestionObject.questionId+"-"+questionObjectAnswer.answerId].push(subRootQuestionObject1.questionId);
           }
          
         }
 
         // if selected object is false we are removing removing objects
         if(questionObjectAnswer.isChosenAnswer === false){
-          const index = this.formQuestions.findIndex((item: any) => item.questionId === subRootQuestionObject1.questionId);
-          if (index !== -1) {
-            // removing subRootQ-2 from formQuestions array
-            this.formQuestions.splice(index, 1);
-            // removing subRootQ-2 from myForm 
-            this.myForm.removeControl(subRootQuestionObject1.questionId);
+
+          // looping through flat Array
+          for (let key in this.subRootQuestionsFlatArray) {
+            // console.log(`${key}: ${this.subRootQuestionsFlatArray[key]}`);
+
+            // simple use case of hiding one level deep questionIds inside the flat array
+            if(key.split("-")[0] === rootQuestionObject.questionId && key.split("-")[1] === questionObjectAnswer.answerId){
+              
+              console.log(this.subRootQuestionsFlatArray[key]);
+              for(const childrenQuestionIds of this.subRootQuestionsFlatArray[key]){
+                const index = this.formQuestions.findIndex((item: any) => item.questionId === childrenQuestionIds);
+                if (index !== -1) {
+                  // removing subRootQ-2 from formQuestions array
+                  this.formQuestions.splice(index, 1);
+                  // removing subRootQ-2 from myForm 
+                  this.myForm.removeControl(subRootQuestionObject1.questionId);
+                }
+
+
+                // finding siblings
+                for(let innerKey in this.subRootQuestionsFlatArray){
+                  if(innerKey.split("-")[0] === childrenQuestionIds){
+                    for(const innerChildrenQuestionIds of this.subRootQuestionsFlatArray[innerKey]){
+
+                      const index = this.formQuestions.findIndex((item: any) => item.questionId === innerChildrenQuestionIds);
+                      if (index !== -1) {
+                        // removing subRootQ-2 from formQuestions array
+                        this.formQuestions.splice(index, 1);
+                        // removing subRootQ-2 from myForm 
+                        this.myForm.removeControl(subRootQuestionObject1.questionId);
+                      }
+                    }
+                  }
+
+                }
+
+
+
+              }
+            }
           }
         }
       }
--- src/app/forms/hardcoded-forms/hardcoded-forms.component.ts
+++ src/app/forms/hardcoded-forms/hardcoded-forms.component.ts
@@ -1,5 +1,5 @@
 import { Component,ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-hardcoded-forms',
@@ -7,13 +7,178 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./hardcoded-forms.component.scss']
 })
 export class HardcodedFormsComponent {
+  form: FormGroup;
+  formData: any;
 
-  @ViewChild('pcrForm') pcrForm: NgForm;
+  constructor(private formBuilder: FormBuilder) {}
 
-  onSubmit(){
-    console.log(this.pcrForm.value);
-    console.log(this.pcrForm.value.emailAddress);
-    console.log(this.pcrForm.value.password);
-    console.log(this.pcrForm.value.checkMeOut);
+  ngOnInit(): void {
+    // Simulated JSON data
+    this.formData = {
+      "formGroupTitle": "General Information",
+      "formGroupCode": "GI",
+      "sections": [
+        {
+          "sectionTitle": "General",
+          "sequenceNumber": 0,
+          "sectionCode": "GIGNRL",
+          "questions": [
+            {
+              "questionId": "GIID1",
+              "questionText": "Inspection Date 1:",
+              "isRequired": true,
+              "isReadOnly": false,
+              "uiControlType": "date",
+              "answers": [
+                {
+                  "answerId": "GIID11",
+                  "answerText": "GIID11",
+                  "isChosenAnswer": false,
+                  "questions": []
+                }
+              ]
+            },
+            {
+              "questionId": "GIID2",
+              "questionText": "Inspection Date 2:",
+              "isReadOnly": false,
+              "uiControlType": "date",
+              "answers": [
+                {
+                  "answerId": "GIID21",
+                  "answerText": "GIID21",
+                  "isChosenAnswer": false,
+                  "questions": []
+                }
+              ]
+            },
+            {
+              "questionId": "GIII",
+              "questionText": "Inspector Unique ID:",
+              "isRequired": true,
+              "isReadOnly": false,
+              "uiControlType": "text",
+              "responseMaxLen": 25,
+              "answers": [
+                {
+                  "answerId": "GIII",
+                  "answerText": "",
+                  "isChosenAnswer": false,
+                  "questions": []
+                }
+              ]
+            },
+            {
+              "questionId": "GICRA",
+              "questionText": "Corrected Address:",
+              "questionType": "QuestionAndSingleAnswerRadioButton",
+              "isRequired": true,
+              "isReadOnly": false,
+              "answers": [
+                {
+                  "answerId": "GICRA1",
+                  "answerText": "Yes",
+                  "isChosenAnswer": true,
+                  "questions": [
+                    {
+                      "questionId": "GIA1",
+                      "questionText": "Address 1:",
+                      "isRequired": false,
+                      "isReadOnly": false,
+                      "uiControlType": "text",
+                      "answers": [
+                        {
+                          "answerId": "GIA11",
+                          "answerText": "1234 Elm St",
+                          "isChosenAnswer": false,
+                          "questions": []
+                        }
+                      ],
+                      "parent": {
+                        "answerId": "GICRA1"
+                      }
+                    },
+                    {
+                      "questionId": "GIA2",
+                      "questionText": "Address 2:",
+                      "isRequired": false,
+                      "isReadOnly": false,
+                      "uiControlType": "text",
+                      "answers": [
+                        {
+                          "answerId": "GIA21",
+                          "answerText": "",
+                          "isChosenAnswer": false,
+                          "questions": []
+                        }
+                      ],
+                      "parent": {
+                        "answerId": "GICRA1"
+                      }
+                    },
+                    {
+                      "questionId": "GICI",
+                      "questionText": "City:",
+                      "isRequired": false,
+                      "isReadOnly": false,
+                      "uiControlType": "text",
+                      "answers": [
+                        {
+                          "answerId": "GICI1",
+                          "answerText": "",
+                          "isChosenAnswer": false,
+                          "questions": []
+                        }
+                      ],
+                      "parent": {
+                        "answerId": "GICRA1"
+                      }
+                    }
+                  ]
+                },
+                {
+                  "answerId": "GICRA2",
+                  "answerText": "No",
+                  "isChosenAnswer": false,
+                  "questions": []
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    };
+
+    this.form = this.formBuilder.group({});
+    this.buildForm(this.formData.sections);
+  }
+
+  buildForm(sections: any[]): void {
+    for (const section of sections) {
+      for (const question of section.questions) {
+        let formControl = null;
+        if (question.uiControlType === 'date') {
+          formControl = new FormControl(question.answers[0]?.answerText || '', question.isRequired ? Validators.required : null);
+        } else if (question.uiControlType === 'text') {
+          formControl = new FormControl(question.answers[0]?.answerText || '', question.isRequired ? Validators.required : null);
+        } else if (question.uiControlType === 'radio') {
+          for (const answer of question.answers) {
+            const nestedQuestions = answer.questions;
+            for (const nestedQuestion of nestedQuestions) {
+              formControl = new FormControl(nestedQuestion.answers[0]?.answerText || '', nestedQuestion.isRequired ? Validators.required : null);
+              this.form.addControl(nestedQuestion.questionId, formControl);
+            }
+          }
+        }
+
+        if (formControl) {
+          this.form.addControl(question.questionId, formControl);
+        }
+      }
+    }
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value);
   }
 }
